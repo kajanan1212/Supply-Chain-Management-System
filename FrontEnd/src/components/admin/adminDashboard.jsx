a2473406364f87c62432b42dfbb1672dba4af669
Sivakajan-tech <my.otherwrks@gmail.com>
2022-05-17 10:47:09 +0530

    branch2

---
+++ FrontEnd/src/components/admin/adminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AdminDashboard extends React.Component {
+    state = {
+        customer: []
+    }
+
+    render() {
+        return (null)
+    }
+}
+
+export default AdminDashboard;
