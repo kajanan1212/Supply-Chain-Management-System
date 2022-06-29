c1a6d6240ab9f5a37056f67c58176e0b80f7cf02
Sivakajan-tech <my.otherwrks@gmail.com>
2022-06-29 23:14:40 +0530

    PDF for QSR

--- FrontEnd/src/components/admin/adminDashboard.jsx
+++ FrontEnd/src/components/admin/adminDashboard.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
+import adminServices from '../service/admin.service';
 
 
 class AdminDashboard extends Component {
-
-
+    state = {
+        name: 'admin'
+    }
     render() {
         return (
             <div className='mx-4 my-4'>
@@ -41,7 +45,7 @@ class AdminDashboard extends Component {
                         </div>
                         <div className="col mx-5 my-4 rounded-3  border border-3 border-dark">
                             <h1 className='pt-5'>QSR</h1>
-                            <button className='btn btn-primary mb-4 mt-4 rounded-pill px-5'>
+                            <button className='btn btn-primary mb-4 mt-4 rounded-pill px-5' onClick={this.createAndDownloadPdf}>
                                 Calculate QSR
                             </button>
                         </div>
@@ -49,6 +53,18 @@ class AdminDashboard extends Component {
                 </div>
             </div >
         );
+    }
+    createAndDownloadPdf = () => {
+        adminServices.pdfparse(this.state)
+            .then(() => axios.get('http://localhost:3001/admin', { responseType: 'blob' }))
+            .then((res) => {
+                console.log(res.data)
+                const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+                console.log(pdfBlob)
+                // saveAs(res.data, 'new.txt');
+                saveAs(pdfBlob, 'newPdf.pdf');
+                // new Blob([res.data], { type: 'application/pdf' }),`sample.pdf`);
+            })
     }
 }
 
--- FrontEnd/src/components/store/storeDashboard.jsx
+++ FrontEnd/src/components/store/storeDashboard.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import storeService from '../service/store.service';
 
 
 class StoreManager extends Component {
-
-
+    state = {
+        totalRoutes: 0,
+        newOrdersOntrain: 'tobeset',
+        totalTruck: 0,
+        totalworkers: 0
+    }
+    async componentDidMount() {
+        // console.log(this.props.store_id)
+        await storeService.getDetails(this.props.store_id).then((response) => {
+            this.setState({
+                totalRoutes: response.data[0][0]['data'], totalTruck: response.data[1][0]['data'], totalworkers: response.data[2][0]['data'], newOrdersOntrain: response.data[3][0]['data']
+            })
+            // window.location.reload()
+        });
+        // console.log(this.state)
+    }
     render() {
         return (
             <div className='mx-4 my-4'>
@@ -13,7 +28,7 @@ class StoreManager extends Component {
                     <div className="row  text-center">
                         <div className="col mx-5 my-4 rounded-3  border border-3 border-dark">
                             <h5 className='pt-4'>Total Routes</h5>
-                            <h1>35</h1>
+                            <h1>{this.state.totalRoutes}</h1>
                             <Link to="/store/setroutes">
                                 <button className='btn btn-primary mb-4 mt-3 rounded-pill px-5'>
                                     Set Routes
@@ -21,8 +36,8 @@ class StoreManager extends Component {
                             </Link>
                         </div>
                         <div className="col mx-5 my-4 rounded-3  border border-3 border-dark">
-                            <h5 className='pt-4'>Total Trains</h5>
-                            <h1>21</h1>
+                            <h5 className='pt-4'>Total New Orders</h5>
+                            <h1>{this.state.newOrdersOntrain}</h1>
                             <Link to="/store/ordersontrain">
                                 <button className='btn btn-primary mb-4 mt-3 rounded-pill px-5'>
                                     Orders on Train
@@ -33,7 +48,7 @@ class StoreManager extends Component {
                     <div className="row  text-center">
                         <div className="col mx-5 my-4 rounded-3  border border-3 border-dark">
                             <h5 className='pt-4'>Total Trucks</h5>
-                            <h1>18</h1>
+                            <h1>{this.state.totalTruck}</h1>
                             <button className='btn btn-primary mb-2 mt-3 rounded-pill px-5'>
                                 Add New Truck
                             </button>
@@ -46,14 +61,18 @@ class StoreManager extends Component {
                         </div>
                         <div className="col mx-5 my-4 rounded-3  border border-3 border-dark">
                             <h5 className='pt-4'>Total Workers</h5>
-                            <h1>62</h1>
-                            <button className='btn btn-primary mb-2 mt-3 rounded-pill px-5'>
-                                Add New Worker
-                            </button>
+                            <h1>{this.state.totalworkers}</h1>
+                            <Link to="/store/addnewworker">
+                                <button className='btn btn-primary mb-2 mt-3 rounded-pill px-5'>
+                                    Add New Worker
+                                </button>
+                            </Link>
                             <br />
-                            <button className='btn btn-primary mb-4 rounded-pill px-5'>
-                                Workers in Delivery
-                            </button>
+                            <Link to="/store/driverondelivery">
+                                <button className='btn btn-primary mb-4 rounded-pill px-5'>
+                                    Workers in Delivery
+                                </button>
+                            </Link>
                         </div>
                     </div>
                 </div>
