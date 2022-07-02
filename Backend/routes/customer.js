b65da8b5c95a03a3be3604da587f57a88396b51a
Sivakajan-tech <my.otherwrks@gmail.com>
2022-07-02 20:45:17 +0530

    Transaction added

--- Backend/routes/customer.js
+++ Backend/routes/customer.js
@@ -30,31 +30,51 @@ router.post('/history', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const a = req.body.dataa;
-    const user = req.body.currentUser
-    const cost = req.body.cost;
-    const capacity = req.body.capacity;
-    const order_id = getUniqId('ord')
-
-    a.carts.forEach(element => {
-        const sql1 = "UPDATE product SET quantity=? WHERE product_id=?";
-        db.query(sql1, [element.quantity - element.count, element.product_id], (err, result) => {
-        })
-
-        const sql2 = "insert into has(order_id,product_id,count) values(?,?,?)";
-        db.query(sql2, [order_id, element.product_id, element.count], (err, result) => { });
-    });
-    // console.log(user)
-    // const address = a.address + ", " + a.city + ", " + a.district;
-    const sql3 = "insert into places(customer_id,order_id,address,city,district) values(?,?,?,?,?)"
-    db.query(sql3, [user.customer_id, order_id, a.address, a.city, a.district], (err, result) => {
-        // console.log(err);
-        // console.log(result);
-
-    });
-
-    const sql4 = "insert into customer_order(order_id,total_price,capacity) values (?,?,?)";
-    db.query(sql4, [order_id, cost, capacity], (err, result) => {
+    db.getConnection(function (err, connection) {
+        connection.beginTransaction(function (err) {
+            if (err) {
+                throw err;
+            }
+            const a = req.body.dataa;
+            const user = req.body.currentUser
+            const cost = req.body.cost;
+            const capacity = req.body.capacity;
+            const order_id = getUniqId('ord')
+            a.carts.forEach(element => {
+                const sql1 = "UPDATE product SET quantity=? WHERE product_id=?";
+                db.query(sql1, [element.quantity - element.count, element.product_id], (error, result) => {
+                    if (error) {
+                        return connection.rollback(function () {
+                            throw error;
+                        });
+                    }
+                })
+                const sql2 = "insert into has(order_id,product_id,count) values(?,?,?)";
+                db.query(sql2, [order_id, element.product_id, element.count], (error, result) => {
+                    if (error) {
+                        return connection.rollback(function () {
+                            throw error;
+                        });
+                    }
+                });
+            });
+            const sql3 = "insert into places(customer_id,order_id,address,city,district) values(?,?,?,?,?)"
+            db.query(sql3, [user.customer_id, order_id, a.address, a.city, a.district], (error, result) => {
+                if (error) {
+                    return connection.rollback(function () {
+                        throw error;
+                    });
+                }
+            });
+            const sql4 = "insert into customer_order(order_id,total_price,capacity) values (?,?,?)";
+            db.query(sql4, [order_id, cost, capacity], (error, result) => {
+                if (error) {
+                    return connection.rollback(function () {
+                        throw error;
+                    });
+                }
+            });
+        });
     });
 });
 
